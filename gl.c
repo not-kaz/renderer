9f761a70a4933b3ac61a062b657482b71b17baf0
not-kaz <kmicagrizli@protonmail.com>
2022-05-24 14:20:55 +0200

    Split clear buffer functions.

--- gl.c
+++ gl.c
@@ -12,9 +12,13 @@ void gl_init(void)
 	glEnable(GL_DEPTH_TEST);
 }
 
-void gl_clear_framebuffer(float r, float g, float b, float a)
+void gl_set_clear_color(float r, float g, float b, float a)
 {
 	glClearColor(r, g, b, a);
+}
+
+void gl_clear_framebuffer(void)
+{
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
--- gl.h
+++ gl.h
@@ -2,7 +2,8 @@
 #define GL_H
 
 void gl_init(void);
-void gl_clear_framebuffer(float r, float g, float b, float a);
+void gl_set_clear_color(float r, float g, float b, float a);
+void gl_clear_framebuffer(void);
 void gl_set_viewport(int vw, int vh);
 
 #endif
