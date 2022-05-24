8443d94319a71da39383577342409a7422fe7503
not-kaz <kmicagrizli@protonmail.com>
2022-05-24 11:46:42 +0200

    Hello, world!

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include "sdl.h"
 #include "gl.h"
+#include "shader.h"
 
 int main(void)
 {
+	struct shader basic_shd;
+
 	sdl_init();
 	sdl_setup("RENDERER");
 
 	gl_init();
 	gl_set_viewport(sdl_get_win_width(), sdl_get_win_height());
+
+	shader_build(&basic_shd, "../basic_vert.glsl", "../basic_frag.glsl");
 
 	while (1) {
 		sdl_poll_events();
